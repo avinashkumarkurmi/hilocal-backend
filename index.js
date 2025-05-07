9598538392a878250ba4cd9ec54cd8591dedaf04
avinashkumarkurmi <avinashkumar341199@gmail.com>
2025-05-07 12:05:10 +0530

    inital backend for hilocal

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json()); // to parse JSON bodies
+
+// Sample route
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
